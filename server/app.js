29768b7eb79950d9ec0541e6d41fb3959e5638da
etidbury <tidbury@live.com>
2018-07-03 14:20:46 +0100

    tweaks

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * app.js
@@ -22,42 +22,56 @@
  *   https://sailsjs.com/anatomy/app.js
  */
 
-
 // Ensure we're in the project directory, so cwd-relative paths work as expected
 // no matter where we actually lift from.
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
 
-require('./init')();
+require("./init")();
 
 // Attempt to import `sails` dependency, as well as `rc` (for loading `.sailsrc` files).
 let sails;
 let rc;
 try {
-  sails = require('sails');
-  rc = require('sails/accessible/rc');
+  sails = require("sails");
+  rc = require("sails/accessible/rc");
 } catch (err) {
-  console.error('Encountered an error when attempting to require(\'sails\'):');
+  console.error("Encountered an error when attempting to require('sails'):");
   console.error(err.stack);
-  console.error('--');
-  console.error('To run an app using `node app.js`, you need to have Sails installed');
-  console.error('locally (`./node_modules/sails`).  To do that, just make sure you\'re');
-  console.error('in the same directory as your app and run `npm install`.');
+  console.error("--");
+  console.error(
+    "To run an app using `node app.js`, you need to have Sails installed"
+  );
+  console.error(
+    "locally (`./node_modules/sails`).  To do that, just make sure you're"
+  );
+  console.error("in the same directory as your app and run `npm install`.");
   console.error();
-  console.error('If Sails is installed globally (i.e. `npm install -g sails`) you can');
-  console.error('also run this app with `sails lift`.  Running with `sails lift` will');
-  console.error('not run this file (`app.js`), but it will do exactly the same thing.');
-  console.error('(It even uses your app directory\'s local Sails install, if possible.)');
+  console.error(
+    "If Sails is installed globally (i.e. `npm install -g sails`) you can"
+  );
+  console.error(
+    "also run this app with `sails lift`.  Running with `sails lift` will"
+  );
+  console.error(
+    "not run this file (`app.js`), but it will do exactly the same thing."
+  );
+  console.error(
+    "(It even uses your app directory's local Sails install, if possible.)"
+  );
   return;
-}//-•
-
+} //-•
 
 // Start server
-sails.lift();
+//sails.lift();
+const isSailsNextHookDisabled=!(process.env.DISABLE_SAILS_NEXT_HOOK&&process.env.DISABLE_SAILS_NEXT_HOOK.length);
+if (isSailsNextHookDisabled)
+  console.warn("Sails Next.js hook has been disabled!");
 
-// sails.lift(Object.assign(rc('sails'),{
-//   hooks: {
-//     "sails-hook-next":false,
-//     "next":false
-//   }
-// }));
+sails.lift(
+  Object.assign(rc("sails"), {
+    hooks: {
+      next: !isSailsNextHookDisabled
+    }
+  })
+);
