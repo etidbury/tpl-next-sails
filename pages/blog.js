cc0a9884a012c37e05f411e44923a1eb9f95a3d6
etidbury <tidbury@live.com>
2018-07-05 11:33:26 +0100

    fixed flow with eslint

--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,13 @@
+//@flow
+
 
 import React from "react";
 import { connect } from "react-redux";
 import Client from "../lib/Client";
-import axios from 'axios';
+import axios from "axios";
 
-class Blog extends React.Component {
-  timer=false;
-  
+class Blog extends React.Component<*> {
+  timer = false;
 
   static async getInitialProps({ reduxStore, req, params }) {
     const isServer = !!req;
@@ -14,26 +15,27 @@ class Blog extends React.Component {
     let r = {};
 
     r = { ...(await Client.get("api/v1/versions").then(r => r.data)) };
-
-  
     if (isServer && req.param) {
       const articleId = req.param("articleId");
-
 
       r = { articleId, ...r };
 
       //return {articleId,clientVersion,serverVersion}
     }
+    
+
+  
 
     return r;
   }
 
   render() {
-
     return (
       <div>
         Blog{" "}
-        <div className="article-id">{this.props.articleId || "not specified"}</div>
+        <div className="article-id">
+          {this.props.articleId || "not specified"}
+        </div>
         <br />
         Client Version: {this.props.clientVersion || "[n/a]"}
       </div>
